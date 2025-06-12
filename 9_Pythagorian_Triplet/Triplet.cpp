b4e3e17370feaf5ba3c8f7fde826b5432136b1fc
Kovas McCann <Kovasmccann@outlook.com>
2025-06-12 10:12:48 -0500

    after a long sickness we back baby!!!

--- 9_Pythagorian_Triplet/Triplet.cpp
+++ 9_Pythagorian_Triplet/Triplet.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
-        
+    //a < b < c
+    int a, b, c;
 
+    for(int c = 1; c < 998; c++){
+        for(int b = 1; b < c; b++){
+            for(int a = 1; a < b; a++){
+                if(a + b + c == 1000){
+                    if(a*a + b*b == c*c){
+                        cout << a*b*c << "\n"; 
+                    }
+                }
+            }
+        }
+    }
 }
