e3001d822ec3f4cfc5c0148652f8a50d533e8acc
Kovas McCann <Kovasmccann@outlook.com>
2025-07-24 10:38:20 -0500

    adding

---
+++ 18_Path_Sum/PathSum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    string fileloc;
+
+    //Get file flag
+    for(int n = 1; n < argc; n++){ //crude way of doing it
+        if(string(argv[n]) == "-i"){
+           fileloc = argv[n + 1];
+        }
+    }
+
+    cout << "Using:" << fileloc << endl; 
+
+    //Create BST
+
+
+}
