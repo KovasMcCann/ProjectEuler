e3feab7668feb3af454ad8009f0b4b370fcda9d7
Kovas McCann <Kovasmccann@outlook.com>
2025-12-23 16:18:01 -0600

    20

--- 19_Factorial_Digit_Sum/FactorialSum.cpp
+++ 19_Factorial_Digit_Sum/FactorialSum.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
-#include <fstream>
-#include <bits/stdc++.h>
 #include <vector>
-
 using namespace std;
 
-int main(){
-    int num = 5;
-    int factorial = 1;
-    int sum = 0; 
 
-    while(num > 1){
-        factorial = factorial * num;
-        num--;
+int n = 10;
+
+int main() {
+    vector<int> digits(1, 1);
+    int sum = 0;
+
+    for (int i = 2; i <= n; i++) {
+        int carry = 0;
+        for (int &d : digits) {
+            int prod = d * i + carry;
+            d = prod % 10;
+            carry = prod / 10;
+        }
+        while (carry) {
+            digits.push_back(carry % 10);
+            carry /= 10;
+        }
     }
 
-    cout << factorial << "\n"; 
+    for (int d : digits)
+        sum += d;
 
-    int len = to_string(num).length();
-    cout << len;
-
-    while(len > 0){
-        int cache = factorial % (len * 10);
-        cout << "cache:" << cache;
-        sum = sum + cache;
-        len--;
-    }
-    cout << sum;
+    cout << "Sum: " << sum;
 }
