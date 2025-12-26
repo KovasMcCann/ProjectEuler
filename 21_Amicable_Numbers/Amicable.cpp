3458a97c9ca3ab7d17487cbbc493dec98e396b56
Kovas McCann <Kovasmccann@outlook.com>
2025-12-25 22:52:14 -0600

    christmas

---
+++ 21_Amicable_Numbers/Amicable.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n = 10;
+
+int main(){
+    /* Need to learn how to use vectors to store data
+    this was used for the last two problems but i was unable to implement myself
+    */
+   
+   vector<int> numbers(11,1); //defines a vector of one element of value 1 
+
+
+   
+   for(int x : numbers){
+       cout << x << "\n";
+   } 
+
+}
---
+++ 21_Amicable_Numbers/Vector.md
@@ -0,0 +1,34 @@
+Vector Operations 
+Get Element
+
+    numbers[n]; //From Zero
+
+First Element
+
+    .front()
+
+Last Element
+
+    .back()
+
+N Element
+
+    .at(n)
+
+Change Element
+
+    numbers[n] = 1
+
+    numbers.at(n) = 1
+
+Add Element
+
+    .push_back(new number) //@ end
+
+Remove 
+
+    .pop_back() //end vector
+
+Size
+
+    .size()
