81a44a2c19ece3ea28e442e70267da2553fc70e8
Kovas McCann (Gentoo PC) <KovasMcCann@outlook.com>
2026-01-08 21:59:32 -0600

    got er doneg++ -o Amicable Amicable.cpp && ./Amicable

--- 21_Amicable_Numbers/Amicable.cpp
+++ 21_Amicable_Numbers/Amicable.cpp
@@ -3,43 +3,43 @@
 
 using namespace std;
 
-int n = 220; //lowest amicable number
+//int n = 220; //lowest amicable number
+int n = 1; //lowest amicable number
 
-void amicable(int n){
+int amicable(int n){
    int sum = 0;
 
    for(int d = n; d > 0; d--){
    	if ( n % d == 0 && d != n){
-		cout << ": " << d << "\n";
 		sum += d;
 	}
-   } //284
+   }
+
+   int csum = 0;
 
    for(int d = sum; d > 0; d--){
-   	if (n % d == 0 && d != n){
-		cout << ": " << d << "\n";
-		sum -= d;
+   	if (sum % d == 0 && d != sum){
+		csum += d;
 	}
    }
 
-   if (sum == n) {
-	cout << "Pair:" << sum << ":" << n << "<~~~~\n";
+   if (n == csum && sum != csum) {
+	cout << "Hit:" << n << "\n";
+	return n;
    }
-   
-   cout << "-----\n" << sum << "\n";
+   else{
+	return 0;
+   }
 }
 
 int main(){
-    /* Need to learn how to use vectors to store data
-    this was used for the last two problems but i was unable to implement myself
-    */
-   
-   vector<int> numbers(11,1); //defines a vector of one element of value 1 
+   int total = 0;
 
-   //while (n < 10000){
-   while (n < 223){
-       cout << "\n" << n << "\n";
-       amicable(n);
+   while (n < 10000){
+       //cout << "\n" << n << "\n";
+       total += amicable(n);
        n++;
    } 
+
+   cout << "------\n"<< total << "\n";
 }
