4cd1076398854cd8c6e6a06c377db2d54a89e842
Kovas McCann (Gentoo PC) <KovasMcCann@outlook.com>
2026-01-09 11:41:11 -0600

    boring stuff done

---
+++ 22_Names_Scores/Names.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> names;
+
+int main(int argc, char** argv){
+	string fileloc;
+
+	for (int n = 1; n < argc; n++) {
+        	if (string(argv[n]) == "-f" && n + 1 < argc) {
+            	fileloc = argv[n + 1];
+            	break;
+        	}
+    	}
+    	cout << "Using:" << fileloc << "\n";
+
+	ifstream Text(fileloc);
+
+	string line;
+    	while (getline(Text, line)) {
+        	stringstream ss(line);
+        	string name;
+
+        	while (getline(ss, name, ',')) {
+			name.erase(remove(name.begin(), name.end(), '"'), name.end());
+            		names.push_back(name);
+            		cout << name << "\n";
+        	}
+    	}
+
+    	Text.close();
+	
+}
