78ca2e5ff3087e3bef1ec159f2db212303df6edf
Kovas McCann (Gentoo PC) <KovasMcCann@outlook.com>
2026-01-09 13:27:21 -0600

    need to create sorting algorithm

--- 22_Names_Scores/Names.cpp
+++ 22_Names_Scores/Names.cpp
@@ -3,10 +3,42 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 vector<string> names;
+
+map<char, int> cpos = {
+	{'A', 1},
+	{'B', 2},
+	{'C', 3},
+	{'D', 4},
+	{'E', 5},
+	{'F', 6},
+	{'G', 7},
+	{'H', 8},
+	{'I', 9},
+	{'J', 10},
+	{'K', 11},
+	{'L', 12},
+	{'M', 13},
+	{'N', 14},
+	{'O', 15},
+	{'P', 16},
+	{'Q', 17},
+	{'R', 18},
+	{'S', 19},
+	{'T', 20},
+	{'U', 21},
+	{'V', 22},
+	{'W', 23},
+	{'X', 24},
+	{'Y', 25},
+	{'Z', 26}
+};
+
+int total = 0;
 
 int main(int argc, char** argv){
 	string fileloc;
@@ -27,7 +59,7 @@ int main(int argc, char** argv){
         	string name;
 
         	while (getline(ss, name, ',')) {
-			name.erase(remove(name.begin(), name.end(), '"'), name.end());
+			name.erase(remove(name.begin(), name.end(), '"'), name.end()); //####
             		names.push_back(name);
             		cout << name << "\n";
         	}
@@ -36,6 +68,19 @@ int main(int argc, char** argv){
     	Text.close();
 
 	//need to create a sorting algorithm
-	//look up table for alphabetical order
 
+	int count = 1;
+	
+	for(string name: names){
+		int worth = 0;
+		for(int i = 0; i < name.size(); i++){
+			worth += cpos[name[i]];
+		}
+		
+		cout << count << ":" << worth << " / " << name << "\n";
+		total += count * worth;
+		count++;
+	}
+
+	cout << "---------\n" << total << "\n";
 }
